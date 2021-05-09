8c26ac0d521f5a53339f923426e301dfd16e7406
Vincent Piegsa <33578820+VincentPiegsa@users.noreply.github.com>
2021-05-09 20:12:15 +0200

    Add files via upload

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,24 +1,75 @@
+"""
+Neural Network Concept
+"""
 import numpy as np 
 from scipy import special
+from keras.datasets import mnist
+import datetime
 
 
 class Utils:
 
+	"""
+	Collection of useful functions
+	"""
+	
 	@staticmethod
 	def sigmoid(x: float) -> float:
+		"""
+		Sigmoid Activation Function (Logistic Growth)
 
+		sigma(x) = 1 / (1 + exp(-x))
+		sigma'(x) = sigma(x) * (1 - sigma(x))
+		
+		Args:
+		    x (float): x
+		
+		Returns:
+		    float: sigmoid(x)
+		"""
 		return special.expit(x)
 
 	@staticmethod
 	def tanh(x: float) -> float:
+		"""
+		Tangens Hyperbolicus Activation Function
 
+		tanh(x) = 1 - 2 / (exp(2x) + 1)
+		tanh'(x) = 1 / cosh(x)^2
+		
+		Args:
+		    x (float): x
+		
+		Returns:
+		    float: tanh(x)
+		"""
 		return np.tanh(x)
 
 
 class Layer(object):
 
+	"""
+	Layer Class, contains basic layer structure
+	
+	Attributes:
+	    activation_function (function): Activation Function
+	    dimension (int): Number of Perceptrons
+	    error (list): Error Vector (Gradient)
+	    id (TYPE): Position of Layer in Neural Network
+	    input (list): Input Vector
+	    output (list): Output Vector
+	"""
+	
 	def __init__(self, dimension: int, id: int, *args, **kwargs):
-
+		"""
+		Initialize Layer
+		
+		Args:
+		    dimension (int): Number of Perceptrons
+		    id (int): Position of Layer in Neural Network
+		    *args: Arguments
+		    **kwargs: Keyword Arguments
+		"""
 		self.dimension = dimension
 		self.activation_function = lambda x: Utils.sigmoid(x)
 
@@ -29,31 +80,81 @@ class Layer(object):
 		self.error = []
 
 	def __repr__(self):
-
+		"""
+		String Representation
+		
+		Returns:
+		    str: Description
+		"""
 		return f"Layer object: {self.dimension} Perceptrons"
 
-	def process(self, input, weight_matrix):
-
+	def process(self, input: np.array, weight_matrix: np.array):
+		"""
+		Process input data
+		
+		Args:
+		    input (np.array): Input Vector
+		    weight_matrix (np.array): Weight Matrix
+		"""
 		self.input = input
 		self.output = self.activation_function(np.dot(weight_matrix, input))
 
 
 class WeightMatrix(object):
 
+	"""
+	Weight Matrix Class, contains the weights between two layers
+	
+	Attributes:
+	    dimensions (tuple): Dimensions of Matrix
+	    matrix (np.array): Weight Matrix
+	"""
+	
 	def __init__(self, dimensions: tuple, *args, **kwargs):
-
+		"""
+		Initialize Weight Matrix
+		
+		Args:
+		    dimensions (tuple): Dimensions of Matrix
+		    *args: Arguments
+		    **kwargs: Keyword Arguments
+		"""
 		self.dimensions = dimensions
 		self.matrix = np.random.normal(0.0, pow(self.dimensions[1], -0.5), self.dimensions)
 
 	def __repr__(self):
-
+		"""
+		String Representation
+		
+		Returns:
+		    str: Description
+		"""
 		return f"WeightMatrix object: {self.dimensions[0]} x {self.dimensions[1]} Weights"
 
 
 class NeuralNetwork(object):
 
+	"""
+	Neural Network Class, contains the Layers, Weights and Learning Algorithms
+	
+	Attributes:
+	    layer_dimensions (list): List of Perceptrons per Layer
+	    layers (list): List of Layers
+	    learning_rate (float): Learning Rate
+	    num_layers (int): Number of Layers
+	    weight_matrices (list): List of Weight Matrices
+	"""
+	
 	def __init__(self, layer_dimensions: list, learning_rate: int, *args, **kwargs):
+		"""
+		Initialize Neural Network
 		
+		Args:
+		    layer_dimensions (list): List of Perceptrons per Layer
+		    learning_rate (int): Learning Rate
+		    *args: Arguments
+		    **kwargs: Keyword Arguments
+		"""
 		self.layer_dimensions = layer_dimensions
 		self.num_layers = len(self.layer_dimensions)
 
@@ -70,11 +171,22 @@ class NeuralNetwork(object):
 		self.learning_rate = learning_rate
 
 	def __repr__(self):
-
+		"""
+		String Representation
+		
+		Returns:
+		    str: Description
+		"""
 		return f"NeuralNetwork object: {self.num_layers} Layers"
 
 	def train(self, input: np.array, target: np.array):
-
+		"""
+		Train the Neural Network with labelled input data
+		
+		Args:
+		    input (np.array): Input Vector
+		    target (np.array): Label Vector
+		"""
 		input = np.array(input, ndmin=2).T
 		target = np.array(target, ndmin=2).T
 		
@@ -92,7 +204,15 @@ class NeuralNetwork(object):
 			self.weight_matrices[layer].matrix += self.learning_rate * np.dot((self.layers[layer + 1].error * self.layers[layer + 1].output * (1 - self.layers[layer + 1].output)), self.layers[layer].output.T)
 
 	def query(self, input: np.array) -> np.array:
-
+		"""
+		Process an input vector without label
+		
+		Args:
+		    input (np.array): Input Vector
+		
+		Returns:
+		    np.array: Output Vector
+		"""
 		input = np.array(input, ndmin=2).T
 		
 		self.layers[0].output = input
